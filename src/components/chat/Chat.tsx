7ea5295dbd6933995021bde4e30a79470e4cd875
JoseVitorNobreUFC <165806736+JoseVitorNobreUFC@users.noreply.github.com>
2024-10-04 01:02:08 +0000

    finalizei a estrutura do front

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -12,8 +12,67 @@ function Chat () {
           </div>
         </div>
       </div>
-      <div className="body"></div>
-      <div className="bottom"></div>
+      <div className="body">
+        <div className="message own">
+          <div className="texts">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, velit quibusdam. Officia est a ad dolorum et. Totam distinctio laboriosam fugit molestiae voluptate, consequuntur enim non quis est fugiat commodi!</p>
+            <span>1 minuto atrás</span>
+          </div>
+        </div>
+        <div className="message">
+          <img src="./iconBot.png" alt="" />
+          <div className="texts">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, velit quibusdam. Officia est a ad dolorum et. Totam distinctio laboriosam fugit molestiae voluptate, consequuntur enim non quis est fugiat commodi!</p>
+            <span>1 minuto atrás</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, velit quibusdam. Officia est a ad dolorum et. Totam distinctio laboriosam fugit molestiae voluptate, consequuntur enim non quis est fugiat commodi!</p>
+            <span>1 minuto atrás</span>
+          </div>
+        </div>
+        <div className="message">
+          <img src="./iconBot.png" alt="" />
+          <div className="texts">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, velit quibusdam. Officia est a ad dolorum et. Totam distinctio laboriosam fugit molestiae voluptate, consequuntur enim non quis est fugiat commodi!</p>
+            <span>1 minuto atrás</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, velit quibusdam. Officia est a ad dolorum et. Totam distinctio laboriosam fugit molestiae voluptate, consequuntur enim non quis est fugiat commodi!</p>
+            <span>1 minuto atrás</span>
+          </div>
+        </div>
+        <div className="message">
+          <img src="./iconBot.png" alt="" />
+          <div className="texts">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, velit quibusdam. Officia est a ad dolorum et. Totam distinctio laboriosam fugit molestiae voluptate, consequuntur enim non quis est fugiat commodi!</p>
+            <span>1 minuto atrás</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, velit quibusdam. Officia est a ad dolorum et. Totam distinctio laboriosam fugit molestiae voluptate, consequuntur enim non quis est fugiat commodi!</p>
+            <span>1 minuto atrás</span>
+          </div>
+        </div>
+        <div className="message">
+          <img src="./iconBot.png" alt="" />
+          <div className="texts">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, velit quibusdam. Officia est a ad dolorum et. Totam distinctio laboriosam fugit molestiae voluptate, consequuntur enim non quis est fugiat commodi!</p>
+            <span>1 minuto atrás</span>
+          </div>
+        </div>
+      </div>
+      <div className="bottom">
+        <input type="text" placeholder="Escreva sua mensagem" />
+        <div className="icons">
+          <img src="./upload.png" alt="" />
+        </div>
+        <button className='sendButton'>Enviar</button>
+      </div>
     </div>
   )
 }
